d09f9044bcc3a32b8840bba5175fd1388c3a9b52
yppelu <yppelu@gmail.com>
2024-08-25 15:36:22 +0700

    Add option to close menu on click outside it

--- client/src/components/room-window/users-list/UsersList.jsx
+++ client/src/components/room-window/users-list/UsersList.jsx
@@ -11,61 +11,63 @@ export default function UsersList({ isMenuAboutToBeHidden, hideMenu, usersInRoom
   const [searchInputValue, setSearchInputValue] = useState('');
 
   return (
-    <div
-      className={
-        isMenuAboutToBeHidden
-          ? 'room-window__users room-window__users--hidden'
-          : 'room-window__users '
-      }
-    >
-      <div className="room-window__users-header">
-        <form
-          className="room-window__search-for-user-form"
-          onSubmit={(event) => {
-            event.preventDefault();
-          }}
-        >
-          <input
-            className="room-window__username-to-search-input"
-            type="text"
-            name="username"
-            placeholder="Search for a user"
-            value={searchInputValue}
-            onChange={(event) => {
-              setSearchInputValue(event.target.value);
-            }}
-          />
-        </form>
-        <ShowHideMenuButton action={hideMenu} />
-      </div>
-      <ul>
-        {
-          usersInRoom
-            .filter(user => user.username.toLowerCase().includes(searchInputValue.toLowerCase()))
-            .map(user => (
-              <li key={user.id} className="room-window__list-of-users-item">
-                <p>{user.username}</p>
-                {isCurrentUserAdmin && user.id !== socket.id &&
-                  <button
-                    type="button"
-                    className="room-window__remove-user-button"
-                    onClick={() => {
-                      socket.emit('removeUser', user.id, user.room);
-                    }}
-                  >
-                    🞪
-                  </button>
-                }
-              </li>
-            ))
+    <>
+      <div
+        className={
+          isMenuAboutToBeHidden
+            ? 'room-window__users room-window__users--hidden'
+            : 'room-window__users '
         }
-      </ul>
-    </div>
+      >
+        <div className="room-window__users-header">
+          <form
+            className="room-window__search-for-user-form"
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+          >
+            <input
+              className="room-window__username-to-search-input"
+              type="text"
+              name="username"
+              placeholder="Search for a user"
+              value={searchInputValue}
+              onChange={(event) => {
+                setSearchInputValue(event.target.value);
+              }}
+            />
+          </form>
+          <ShowHideMenuButton action={hideMenu} />
+        </div>
+        <ul>
+          {
+            usersInRoom
+              .filter(user => user.username.toLowerCase().includes(searchInputValue.toLowerCase()))
+              .map(user => (
+                <li key={user.id} className="room-window__list-of-users-item">
+                  <p>{user.username}</p>
+                  {isCurrentUserAdmin && user.id !== socket.id &&
+                    <button
+                      type="button"
+                      className="room-window__remove-user-button"
+                      onClick={() => {
+                        socket.emit('removeUser', user.id, user.room);
+                      }}
+                    >
+                      🞪
+                    </button>
+                  }
+                </li>
+              ))
+          }
+        </ul>
+      </div>
+      <div className="room-window__users-hide-on-click-block" onClick={hideMenu}></div>
+    </>
   );
 }
 
 UsersList.propTypes = {
-  isMenuHidden: PropTypes.bool,
   isMenuAboutToBeHidden: PropTypes.bool,
   hideMenu: PropTypes.func,
   usersInRoom: PropTypes.array,
